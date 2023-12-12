7b464d093bb69e12a1cddc51f90d94f79d166be8
AniaLaz <lazarenko.ania.sg@gmail.com>
2023-12-12 15:03:02 +0200

    cline

--- controllers/photos.js
+++ controllers/photos.js
@@ -1,4 +1,4 @@
-const {Photo} = require("../models/photo");
+const { Photo } = require("../models/photo");
 
 const { HttpErorr, ctrlWrapper } = require("../helpers");
 
@@ -6,40 +6,21 @@ const getAll = async (req, res) => {
   const { page = 1, limit = 3 } = req.query;
   console.log(page);
   const skip = (page - 1) * limit;
-  
-  const result = await Photo.find({},"",{
-      skip,
-      limit,
-    });
+
+  const result = await Photo.find({}, "", {
+    skip,
+    limit,
+  });
   res.json(result);
 };
 
 const getLove = async (req, res) => {
-    const { page = 1, limit = 4 } = req.query;
-  console.log(page);
-  console.log("limit", limit);
-  console.log("res", req);
-    const skip = (page - 1) * limit;
-
-  const result = await Photo.find({
-    category: "love" }, "", {
-      skip,
-      limit,
-    });
-  res.json(result);
-};
-
-
-const getChildren = async (req, res) => {
   const { page = 1, limit = 4 } = req.query;
-  console.log(page);
-  console.log("limit", limit);
-  console.log("res", req);
   const skip = (page - 1) * limit;
 
   const result = await Photo.find(
     {
-      category: "children",
+      category: "love",
     },
     "",
     {
@@ -50,17 +31,26 @@ const getChildren = async (req, res) => {
   res.json(result);
 };
 
+const getChildren = async (req, res) => {
+  const { page = 1, limit = 4 } = req.query;
+  const skip = (page - 1) * limit;
 
+  const result = await Photo.find({ category: "children" }, "", {
+    skip,
+    limit,
+  });
+  res.json(result);
+};
 
 const getById = async (req, res) => {
-    const { id } = req.params;
-    const result = await Photo.findOne({ _id: id });
-    if (!result) {
-      throw HttpErorr(404, "Not found");
-    }
-    res.json(result);
+  const { id } = req.params;
+  const result = await Photo.findOne({ _id: id });
+  if (!result) {
+    throw HttpErorr(404, "Not found");
+  }
+  res.json(result);
 };
- 
+
 const add = async (req, res) => {
   console.log("req.body", req.bod);
   const newPost = await Photo.create({ ...req.body });
@@ -68,17 +58,15 @@ const add = async (req, res) => {
 };
 
 const deleteById = async (req, res, next) => {
-   const contactDelete = await Photo.findOneAndRemove({
-     _id: req.params.id,
-   });
+  const contactDelete = await Photo.findOneAndRemove({
+    _id: req.params.id,
+  });
   if (!contactDelete) {
     throw HttpErorr(404, "not found");
   } else {
     res.json({ message: "contact deleted", status: "200" });
   }
 };
-
-
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
